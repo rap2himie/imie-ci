095449e9d031085444ea9194f8e341aa42724e15
rap2h <raphaelht@gmail.com>
2017-05-08 11:29:08 +0200

    hop

--- test/FinderTest.php
+++ test/FinderTest.php
@@ -9,6 +9,7 @@ use \SimilarText\Finder;
 class FinderTest extends \PHPUnit_Framework_TestCase
 {
     public function testOk() {
+        //
         $text_finder = new Finder('bananna', ['apple', 'kiwi', 'banana', 'orange', 'banner']);
         $this->assertEquals('banana', $text_finder->first());
     }
